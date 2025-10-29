1ffbe3c3a501c715f2cc34463d4a7cffb014ea8e
hu <dh97249@gmail.com>
2025-10-29 10:38:07 +0900

    feat: add pagination logic

--- api/routes/articles.py
+++ api/routes/articles.py
@@ -1,16 +1,29 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from sqlalchemy.orm import Session
+
+from db.schemas import ArticleBase, Pagination
 from db.session import get_db
 from db import crud
 
 router = APIRouter()
 
-@router.get("/")
-def get_articles(db: Session = Depends(get_db)):
-    """DB에 저장되어 있는 인기글 목록 반환"""
-    return crud.get_all_articles(db)
+@router.get("/", response_model=Pagination)
+def get_articles(
+        page: int = Query(1, ge=1), # page만 입력
+        db: Session = Depends(get_db)
+):
+    """DB에 저장되어 있는 인기글 목록 반환(페이지네이션 포함)"""
+    limit = 20 # limit 내부 고정(url 깔끔)
+    offset = (page - 1) * limit
+    return crud.get_all_articles(db, offset = offset, limit = limit)
 
-@router.get("/sitename/{siteName}")
-def get_article_sitename(siteName: str, db: Session = Depends(get_db)):
-    """사이트별 목록 반환"""
-    return crud.get_articles_sitename(db, siteName)
+@router.get("/sitename/{siteName}", response_model=Pagination)
+def get_article_sitename(
+        siteName: str,
+        page: int = Query(1, ge = 1),
+        db: Session = Depends(get_db),
+):
+    """사이트별 목록 반환 (페이지네이션 포함)"""
+    limit = 20
+    offset = (page - 1) * limit
+    return crud.get_articles_sitename(db, siteName, offset = offset, limit = limit)
--- db/crud.py
+++ db/crud.py
@@ -1,7 +1,31 @@
+from sqlalchemy.orm import Session
+
 from db.models import Article
 
-def get_all_articles(db):
-    return db.query(Article).order_by(Article.collected_date.desc()).all()
+def get_all_articles(db, offset=0, limit=20):
+    total = db.query(Article).count()
+    items = (
+        db.query(Article)
+         .order_by(Article.creation_date.desc())
+         .offset(offset)
+         .limit(limit)
+         .all()
+     )
+    return {"total": total, "page": offset // limit + 1, "items": items}
 
-def get_articles_sitename(db, siteName):
-    return db.query(Article).filter(Article.siteName == siteName).all()
+def get_articles_sitename(
+        db: Session,
+        siteName: str,
+        offset: int = 0,
+        limit: int = 20
+):
+    total = db.query(Article).filter(Article.siteName == siteName).count()
+    items = (
+        db.query(Article)
+        .filter(Article.siteName == siteName)
+        .order_by(Article.creation_date.desc())
+        .offset(offset)
+        .limit(limit)
+        .all()
+    )
+    return {"total": total, "page": offset // limit + 1, "items": items}
